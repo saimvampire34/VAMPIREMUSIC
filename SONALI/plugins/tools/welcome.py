a594c22ade7beec643097436de419af0d5b24fef
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-12-17 18:38:59 +0530

    Update welcome.py

--- SONALI/plugins/tools/welcome.py
+++ SONALI/plugins/tools/welcome.py
@@ -99,7 +99,7 @@ async def greet_new_member(_, member: ChatMemberUpdated):
         
         # Creating an inline button to "Join 👋" with the link
         keyboard = InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Join 👋", url="https://t.me/+czQ48LDbT1cxMWQ1")]]
+            [[InlineKeyboardButton("Join 👋", url="https://t.me/+mq-bBizvpf9mNGZl")]]
         )
 
         # Send the welcome message with the inline button
