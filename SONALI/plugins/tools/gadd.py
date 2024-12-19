5acfda7ab1af1f2a9d436b74b4ffade3f67c5ca9
TEAM ISTKHAR <mdistkhar1078@gmail.com>
2024-12-19 08:30:05 +0530

    Update gadd.py

--- SONALI/plugins/tools/gadd.py
+++ SONALI/plugins/tools/gadd.py
@@ -2,7 +2,7 @@ import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-OWNERS = "7445883361"
+OWNERS = "7995626429"
 from SONALI import app
 from SONALI.utils.database import add_served_chat, get_assistant
 
